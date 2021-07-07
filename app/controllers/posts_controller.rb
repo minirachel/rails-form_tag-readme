9dc0d43f6fe48238fda852bb1805661c94522d57
minirachel <rachel.a.levinson@gmail.com>
2021-07-07 13:07:30 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,14 @@
 class PostsController < ApplicationController
+  
+  def new
+
+  end
+
+  def create
+    Post.create(title: params[:post][:title], description: params[:post][:description])
+    redirect_to posts_path
+  end 
+  
   def index
     @posts = Post.all
   end
